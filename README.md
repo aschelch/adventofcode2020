68ba0765485cd5da2cbb0f5238a371f0eea58ee7
Aurélien Schelcher <aurelien.schelcher@gmail.com>
2020-12-02 20:30:28 +0100

    feat(app): add day2

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 My personal repo for [Advent Of Code 2020](https://adventofcode.com/2020)
 
 :white_check_mark: [Day 1: Report Repair](https://adventofcode.com/2020/day/1)  
-:ballot_box_with_check: Day 2: coming tomorrow...  
+:white_check_mark: [Day 2: Password Philosophy](https://adventofcode.com/2020/day/2)
---
+++ day2/script.py
@@ -0,0 +1,35 @@
+import re
+
+file = open("input.txt", "r")
+lines = [line.rstrip('\n') for line in file]
+
+# count passwords that satify policy (letter contains between x and y times)
+count = 0
+for i in range(len(lines)):
+    res = re.search('([0-9]+)-([0-9]+) ([a-z]): (.*)', lines[i], re.IGNORECASE)
+    
+    x = int(res.group(1))
+    y = int(res.group(2))
+    password = res.group(4)
+    letter = res.group(3)
+
+    occurence = password.count(letter)
+    if x <= occurence and occurence <= y :
+        count+=1
+
+print(count)
+
+# count passwords that satify policy (letter if at least once in position x or y)
+count = 0
+for i in range(len(lines)):
+    res = re.search('([0-9]+)-([0-9]+) ([a-z]): (.*)', lines[i], re.IGNORECASE)
+
+    x = int(res.group(1))
+    y = int(res.group(2))
+    password = res.group(4)
+    letter = res.group(3)
+    
+    if (password[x-1] == letter and password[y-1] != letter) or (password[x-1] != letter and password[y-1] == letter) :
+        count+=1
+        
+print(count)
